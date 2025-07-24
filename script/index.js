054ef241e15456eb04bebbb6050d146114f8bf73
117ABHISHEK <147874160+117ABHISHEK@users.noreply.github.com>
2025-07-24 10:39:31 +0530

    updated
    Co-Authored-By: ShitalPanhalkar1015 <182597102+ShitalPanhalkar1015@users.noreply.github.com>

---
+++ script/index.js
@@ -0,0 +1,7 @@
+//  template/index.js
+document.getElementById("login-register-btn").onclick = function () {
+    window.location.href = "login.html";
+};
+document.getElementById("get-started-btn").onclick = function () {
+    window.location.href = "login.html";
+};
